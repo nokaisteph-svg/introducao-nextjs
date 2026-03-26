b198097afc3067bb5f73da20396a268f8a1b5cab
nokaisteph-svg <nokaistephany@gmail.com>
2026-03-26 08:29:06 -0300

    css
    correçãozinha fora de aula 26/03

--- src/pages/atividades/atv-01/index.jsx
+++ src/pages/atividades/atv-01/index.jsx
@@ -1,8 +1,10 @@
+import styles from './index.module.css'
+
 function Atividade01() {
     return (
         <div className={styles.container}>
             <h1>Atividade01</h1>
-            <p>CSS</p>
+            <h2>CSS</h2>
         </div>
     )
 }
